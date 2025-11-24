48a2604ca64b1a9bdb4a3ed5cc621b362ab5ffd9
Austin Aloway <aaloway@gmail.com>
2025-11-24 12:02:03 -0700

    Make worldedit asset case sensitive

--- tests/test_mim.py
+++ tests/test_mim.py
@@ -112,7 +112,7 @@ def test_main_uninstall(tmp_path):
     assert result == 0
 
     # Verify WorldEdit 7.3.8 was installed
-    qs_matches = glob.glob(os.path.join(tmp_path, 'plugins', "WorldEdit*7.3.8*jar"))
+    qs_matches = glob.glob(os.path.join(tmp_path, 'plugins', "worldedit*7.3.8*jar"))
     assert qs_matches and os.path.isfile(qs_matches[0])
 
     # Update WorldEdit version
@@ -129,7 +129,7 @@ def test_main_uninstall(tmp_path):
     json_file = os.path.join(tmp_path, "install.json")
     with open(json_file, 'w', encoding='utf-8') as f:
         json.dump(data, f)
-        
+
     # Run the install command
     result = mim.main(['install', '--file', json_file, '--destination', tmp_path])
     assert result == 0
@@ -139,10 +139,10 @@ def test_main_uninstall(tmp_path):
     assert qs_matches and os.path.isfile(qs_matches[0])
 
     # Verify WorldEdit 7.3.9 was installed and 7.3.8 was removed
-    qs_matches = glob.glob(os.path.join(tmp_path, 'plugins', "WorldEdit*7.3.9.jar"))
+    qs_matches = glob.glob(os.path.join(tmp_path, 'plugins', "worldedit*7.3.9.jar"))
     assert qs_matches and os.path.isfile(qs_matches[0])
 
-    qs_matches = glob.glob(os.path.join(tmp_path, 'plugins', "WorldEdit*7.3.8.jar"))
+    qs_matches = glob.glob(os.path.join(tmp_path, 'plugins', "worldedit*7.3.8.jar"))
     assert not qs_matches
 
 def test_main_install_dryrun(tmp_path):
